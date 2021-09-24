aee3bda1f11fde1829ce15bda7faa0020400f418
kobayashi <kobayashihonoka41@gmail.com>
2021-09-24 21:07:13 +0900

    add Todo sample

--- src/Component/TestPage/index.js
+++ src/Component/TestPage/index.js
@@ -1,5 +1,56 @@
+import React, { Component, useState, useRef, useEffect} from 'react';
+
+function Todo(){
+const [items, setItems] = useState([{text: 'cat1'}, {text: 'cat2'}]);
+const handleAdd = text =>{
+  setItems([...items, {text}]);
+} 
+return(
+  <div>
+    <p>
+      <Input onAdd={handleAdd} />
+    </p>
+    {items.map(item =>(
+      <p>
+      <TodoItem item={item} />
+      </p>
+    ))}
+    <p>{items.length}</p>
+  </div>
+)
+}
+
+function TodoItem({item}){
+  return(
+    <p>
+    {item.text}
+    </p>
+  );
+}
+
+
+function Input({onAdd}){
+  const [text,setText] = useState('');
+  const handleChange = e => setText(e.target.value);
+  const handleClick = e => {
+    onAdd(text);
+    setText('');
+  }
+  
+  return(
+    <div>
+      <input value={text} onChange={handleChange} />
+      <button onClick={handleClick}>Add</button>
+    </div>
+  )
+}
+
+
+
 export default function TestPage(){
     return (
-        <h1 align="center">test</h1> 
+      <div align="center">
+        <Todo /> 
+      </div>  
   );
 }
